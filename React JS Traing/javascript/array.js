a9b7f7b598af9ccaa1521117edc93e8005a7b156
Mku9204 <66777803+Mku9204@users.noreply.github.com>
2022-07-28 10:51:04 +0530

    Add files via upload

---
+++ React JS Traing/javascript/array.js
@@ -0,0 +1,21 @@
+// let arr=['a','b','c','Abhi','do','with']
+// let arr2=['do','w','323','asd']
+// console.log(arr.join(arr2));
+// arr.push('cassa0')
+// console.log(arr)
+// arr.pop()
+// console.log(arr)
+// arr2.shift()
+// console.log(arr2)
+// console.log(arr2.shift())
+// arr.unshift('hjhh')
+// console.log(arr)
+
+// let arr=['skka','ww','qs']
+// arr.splice(2,1,'fbdf','sdfsf','dfsfsd')
+// console.log(arr)
+
+// let arr=['dsfdf','dfasdf','dsfasdf','werwe']
+// console.log(arr.slice(1))
+// console.log(arr)
+
---
+++ React JS Traing/javascript/index.js
@@ -0,0 +1,45 @@
+//alert("hello Javascript")
+//String fuction
+var str='Hello World';
+
+//slice
+console.log(str.slice(2,6));
+
+//substring
+console.log(str.substring(1,4))
+
+//substr
+console.log(str.substr(0,5))
+
+//concat
+console.log(str.concat([1,2,3,4,5]))
+
+//replace
+console.log(str.replace('Hello','Abhi'))
+
+//reverse
+//console.log(str.reverse())
+
+//trim
+console.log('trim',str.trim())
+
+//charAt
+console.log('charAt',str.charAt(0))
+
+//endsWith
+console.log('endsWith',str.endsWith('a'))
+
+//indexOf
+console.log('indexof',str.indexOf('l'))
+
+//lastIndexOf
+console.log('lastindexof',str .lastIndexOf(0))
+
+//charCodeAt
+console.log('char',str.charCodeAt(0))
+
+//split
+console.log('split',str.split(','))
+
+//lastIndexOf
+console.log('lastindexof',str .lastIndexOf(7))
